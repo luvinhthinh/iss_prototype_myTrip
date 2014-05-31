fc8559cf241415bbb5029b311d1d33373c253100
luvinhthinh <luvinhthinh.mono@gmail.com>
2014-05-31 11:41:51 +0800

    Update distanceGather.py
    Parse the result json and get the distance.

--- distanceGather.py
+++ distanceGather.py
@@ -14,9 +14,12 @@
 #response = conn.getresponse()
 #print response.status, response.reason, response.read()
 
-import urllib2
+import urllib2, json
 req = urllib2.Request('https://maps.googleapis.com/maps/api/directions/json?origin=Singapore+Zoo,Singapore&destination=Underwater+World,Singapore&sensor=false&key=AIzaSyBn3GNyb8I5bUVSP7DdcEN1L_pnLQ6Hk5s', "")
 response = urllib2.urlopen(req)
 result_json = response.read()
 print result_json
 
+j = json.loads(result_json)
+distance = j['routes'][0]['legs'][0]['distance']['value']
+print distance
