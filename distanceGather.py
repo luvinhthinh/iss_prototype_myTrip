1cee07012c223f1ffcdbe96316c51df50ac5c2d2
Mono Lu <luvinhthinh.mono@gmail.com>
2014-06-02 21:21:46 +0800

    Shortest path function on distance

--- distanceGather.py
+++ distanceGather.py
@@ -1,25 +1,38 @@
-#import httplib, urllib, urllib2
+country = 'Singapore'
+places = ['Singapore Zoo', 'Underwater World', 'Night Safari', 'Jurong Bird Park', 'Gardens by the Bay']
+#start = 'Singapore Zoo'
+start = 'Underwater World'
 
-#params = urllib.urlencode({'origin': 'Singapore Zoo', 'destination': 'Underwater World', 'sensor': 'false', 'key':'AIzaSyBn3GNyb8I5bUVSP7DdcEN1L_pnLQ6Hk5s'})
-#print params
-#params2 = ""
-#print params2
+# Contruct distance graph
+# install networkx : https://pypi.python.org/pypi/networkx/
+# install openopt : https://pypi.python.org/pypi/openopt
+# install milp : http://openopt.org/MILP
+# install lpsolve : https://racingtadpole.com/blog/install-lpsolve-for-python/ 
 
-#headers = {"Content-type": "application/json", "Accept":"*/*"}
-#conn = httplib.HTTPSConnection('maps.googleapis.com')
-#conn.connect()
-#conn.request('POST', '/maps/api/directions/json', 'origin=Singapore+Zoo,Singapore&destination=Underwater+World,Singapore&sensor=false&key=AIzaSyBn3GNyb8I5bUVSP7DdcEN1L_pnLQ6Hk5s', {"Content-type": "application/json"})
-#conn.request('POST', '/maps/api/directions/json', params2, headers)
-
-#response = conn.getresponse()
-#print response.status, response.reason, response.read()
-
+import networkx as nx
 import urllib2, json
-req = urllib2.Request('https://maps.googleapis.com/maps/api/directions/json?origin=Singapore+Zoo,Singapore&destination=Underwater+World,Singapore&sensor=false&key=AIzaSyBn3GNyb8I5bUVSP7DdcEN1L_pnLQ6Hk5s', "")
-response = urllib2.urlopen(req)
-result_json = response.read()
-print result_json
+from openopt import *
 
-j = json.loads(result_json)
-distance = j['routes'][0]['legs'][0]['distance']['value']
-print distance
+G = nx.Graph()
+baseurl = 'https://maps.googleapis.com/maps/api/directions/json?'
+src = 'origin='
+des = '&destination='
+otherParams = '&sensor=false&key=AIzaSyBn3GNyb8I5bUVSP7DdcEN1L_pnLQ6Hk5s'
+for i in range(len(places)):
+    for j in range(len(places)):
+        if j<i :
+        	p1 = places[i].replace(' ', '+')
+        	p2 = places[j].replace(' ', '+')
+        	url = baseurl+src+p1+','+country+ des+p2+','+country+otherParams
+        	req = urllib2.Request(url, "")
+        	response = urllib2.urlopen(req)
+        	result_json = response.read()
+        	jsonObj = json.loads(result_json)
+        	distance = jsonObj['routes'][0]['legs'][0]['distance']['value']
+        	G.add_edges_from([(i, j, {'distance':distance})])
+        	
+p = TSP(G, objective = 'distance', start = places.index(start))
+r = p.solve('lpSolve')
+print(r.nodes)
+print(r.edges)
+print(r.Edges)
